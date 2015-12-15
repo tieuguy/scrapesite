5a6ca712b579a453454a02211a594b3d5cb242d4
Philip Tieu <gauryuuki@hotmail.com>
2015-12-15 14:22:53 -0500

    got webpage showing

--- scrapesite/urls.py
+++ scrapesite/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^scrape/', include('scrape.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
